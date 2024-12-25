5f42a60bb65723de3d5a431f6316c17e0e8889c5
Beyza Keser <beyzaaakeser@gmail.com>
2024-12-25 20:06:38 +0300

    update

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,17 @@
 const User = require('../models/user.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('bcryptjs');
+const cloudinary = require('cloudinary').v2;
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
 const register = async (req, res) => {
+  const avatar = await cloudinary.uploader.upload(req.body.avatar, {
+    folder: 'avatars',
+    width: 130,
+    crop: 'scale',
+  });
+
   const { name, email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
@@ -19,7 +28,15 @@ const register = async (req, res) => {
     });
   }
 
-  const newUser = await User.create({ name, email, password: passwordHash });
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+    avatar: {
+      public_id: avatar.public_id,
+      url: avatar.secure_url,
+    },
+  });
 
   const token = await jwt.sign({ id: newUser._id }, 'SECRETTOKEN', {
     expiresIn: '1h',
@@ -74,11 +91,67 @@ const logout = async (req, res) => {
   };
   res
     .status(200)
-    .cookie('token', null,cookieOptions)
+    .cookie('token', null, cookieOptions)
     .json({ message: 'Logged out successfully!' });
 };
 
-const forgotpassword = async (req, res) => {};
+const forgotpassword = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return res.status(500).json({
+      message: 'User not found!',
+    });
+  }
+
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  user.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+  user.resetPasswordExpire = new Date(Date.now() + 5 * 60 * 1000);
+
+  await user.save({ validateBeforeSave: false });
+
+  const passwordUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/reset/${resetToken}`;
+
+  const message = `Token to be used to reset password: ${passwordUrl}`;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      auth: {
+        user: process.env.NODE_AUTH_MAIL,
+        pass: process.env.NODE_AUTH_PASS,
+      },
+      secure: true,
+    });
+
+    const mailData = {
+      from: process.env.NODE_AUTH_MAIL,
+      to: req.body.email,
+      subject: 'Reset Password',
+      text: message,
+    };
+
+    await transporter.sendMail(mailData);
+
+    res.status(200).json({
+      message: 'Password reset link sent to your email',
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const resetpassword = async (req, res) => {};
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -16,9 +16,9 @@ const router = express.Router();
 router.get('/products', allProducts);
 router.get('/admin/products',authenticationMid,rolechecked("admin"), adminProducts);
 router.get('/products/:id', detailProducts);
-router.post('/products/new',authenticationMid, createProduct);
+router.post('/products/new',authenticationMid,rolechecked("admin"), createProduct);
 router.post('/products/newReview',authenticationMid, createReview);
-router.delete('/products/:id',authenticationMid, deleteProduct);
-router.put('/products/:id',authenticationMid, updateProduct);
+router.delete('/products/:id',authenticationMid,rolechecked("admin"), deleteProduct);
+router.put('/products/:id',authenticationMid,rolechecked("admin"), updateProduct);
 
 module.exports = router;
